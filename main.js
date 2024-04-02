cda9b2338571ffb53b6f2eee6636a0898e8094be
Ragnarock1 <raresciocirlan@yahoo.com>
2024-04-02 13:01:08 +0300

    etapa 3

---
+++ main.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector('#hamburger-toggle');
+const nav = document.querySelector('nav');
+const ham = document.querySelector(".hamburger");
+
+navToggle.addEventListener("click", () => {
+    nav.classList.toggle('open');
+    if (nav.classList.contains('open')) {
+        ham.classList.toggle('active');
+    } else {
+        ham.classList.remove('active');
+    }
+});
